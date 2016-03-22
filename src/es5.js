c821635dcdcdd4b11eb9d1adc107158a401667cf
Johnathan Leppert <johnathan@kespry.com>
2016-03-22 16:28:58 -0700

    really fix es5 deps

--- src/es5.js
+++
@@ -1,6 +0,0 @@
-import L from 'leaflet';
-import TransformPolygonWithImageOverlay from './feature/TransformPolygonWithImageOverlay';
-import TransformImageOverlay from './feature/TransformImageOverlay';
-import TransformPolygonWithMarkers from './feature/TransformPolygonWithMarkers';
-
-export { TransformPolygonWithImageOverlay, TransformImageOverlay, TransformPolygonWithMarkers };
